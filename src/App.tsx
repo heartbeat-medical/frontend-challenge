2ea14347c2e2bf7511840abe988c71f04a1a61d5
Apoorv Mittal <apoorv173@yahoo.co.in>
2022-01-31 17:11:06 +0100

    added header and footer components to have a more UX friendly look, all designs and html copied from heartheat homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,43 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { Patient, PatientsService } from "./patients/patients";
 import { createNewPatientsApi } from "./patients/patients_api";
 import PatientsLoader from "./patients/patients_loader_button";
 import PatientsSearch from "./patients/patients_search";
+import Header from "./header";
+import Footer from "./footer";
 
-function App() {
+const App = () => {
   const [patients, updatePatients] = useState<Patient[]>([]);
-
   const [patientsApi] = useState<PatientsService>(
     createNewPatientsApi("http://localhost:3000")
   );
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Heartbeat 🏥</h1>
-        <div
-          style={{
-            border: "1px solid white",
-            borderRadius: "8px",
-            padding: "20px",
-            marginBottom: "20px",
-          }}
-        >
-          <h2>Please load the patients using the button below or search</h2>
-          <PatientsLoader
-            loadPatients={patientsApi.All}
-            onLoaded={updatePatients}
-          />
+      <div className="App-container">
+        <Header />
+        <div className="App-body">
           <PatientsSearch
             loadPatients={patientsApi.Search}
             onResults={updatePatients}
           />
+          <h3>Or</h3>
+          <PatientsLoader
+            loadPatients={patientsApi.All}
+            onLoaded={updatePatients}
+          />
           {patients.length > 0 && displayPatients(patients)}
         </div>
-      </header>
+        <Footer />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
 
-function displayPatients(patients: Patient[]) {
+const displayPatients = (patients: Patient[]) => {
   return (
     <ul>
       {patients.map((p, k) => (
@@ -52,4 +47,4 @@ function displayPatients(patients: Patient[]) {
       ))}
     </ul>
   );
-}
+};
